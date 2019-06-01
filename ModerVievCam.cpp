509f709ffd0efb03693e22529f92f1928641b8a9
Михаил <filmih24@mail.ru>
2019-06-01 09:52:29 +0400

    last changes

--- ModerVievCam.cpp
+++ ModerVievCam.cpp
@@ -1,57 +1,7 @@
 ﻿
 /* Copyright (c) Mark J. Kilgard, 1994. */
 
-/**
- * (c) Copyright 1993, 1994, Silicon Graphics, Inc.
- * ALL RIGHTS RESERVED
- * Permission to use, copy, modify, and distribute this software for
- * any purpose and without fee is hereby granted, provided that the above
- * copyright notice appear in all copies and that both the copyright notice
- * and this permission notice appear in supporting documentation, and that
- * the name of Silicon Graphics, Inc. not be used in advertising
- * or publicity pertaining to distribution of the software without specific,
- * written prior permission.
- *
- * THE MATERIAL EMBODIED ON THIS SOFTWARE IS PROVIDED TO YOU "AS-IS"
- * AND WITHOUT WARRANTY OF ANY KIND, EXPRESS, IMPLIED OR OTHERWISE,
- * INCLUDING WITHOUT LIMITATION, ANY WARRANTY OF MERCHANTABILITY OR
- * FITNESS FOR A PARTICULAR PURPOSE.  IN NO EVENT SHALL SILICON
- * GRAPHICS, INC.  BE LIABLE TO YOU OR ANYONE ELSE FOR ANY DIRECT,
- * SPECIAL, INCIDENTAL, INDIRECT OR CONSEQUENTIAL DAMAGES OF ANY
- * KIND, OR ANY DAMAGES WHATSOEVER, INCLUDING WITHOUT LIMITATION,
- * LOSS OF PROFIT, LOSS OF USE, SAVINGS OR REVENUE, OR THE CLAIMS OF
- * THIRD PARTIES, WHETHER OR NOT SILICON GRAPHICS, INC.  HAS BEEN
- * ADVISED OF THE POSSIBILITY OF SUCH LOSS, HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, ARISING OUT OF OR IN CONNECTION WITH THE
- * POSSESSION, USE OR PERFORMANCE OF THIS SOFTWARE.
- *
- * US Government Users Restricted Rights
- * Use, duplication, or disclosure by the Government is subject to
- * restrictions set forth in FAR 52.227.19(c)(2) or subparagraph
- * (c)(1)(ii) of the Rights in Technical Data and Computer Software
- * clause at DFARS 252.227-7013 and/or in similar or successor
- * clauses in the FAR or the DOD or NASA FAR Supplement.
- * Unpublished-- rights reserved under the copyright laws of the
- * United States.  Contractor/manufacturer is Silicon Graphics,
- * Inc., 2011 N.  Shoreline Blvd., Mountain View, CA 94039-7311.
- *
- * OpenGL(TM) is a trademark of Silicon Graphics, Inc.
- */
- /*----------------------------------------------------------------------------
-  *
-  * olight.c : openGL (motif) example showing how to do hardware lighting
-  *            including two_sided lighting.
-  *
-  * Author : Yusuf Attarwala
-  *          SGI - Applications
-  * Date   : Mar 93
-  *
-  *    press  left   button for animation
-  *           middle button for two sided lighting (default)
-  *           right  button for single sided lighting
-  *
-  *
-  *---------------------------------------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -59,74 +9,106 @@
 #include <cmath>
 #include <iostream>
 
-
+#define GLEW_STATIC
+#include <vector>
+#include <GL/glew.h>
 #include <GL/glut.h>
+#include <SOIL.h>
 #include <GL/freeglut.h>
-#include <GL/GLU.h>
 
   /* function declarations */
 void
-drawScene(void), setMatrix(void), initLightAndMaterial(void),
-animation(void), resize(int w, int h), menu(int choice), keyboard(unsigned char c, int x, int y);
-
+animation_go(void), setMatrix(void), initLight(void), 
+Skybox(void),processSpecialKeys(int key, int xx, int yy), draw(), stay(void), resize(int w, int h), menu(int choice), keyboard(unsigned char c, int x, int y);
 
 /* global variables */
 
-float ax, ay, az;       /* angles for animation */
-GLUquadricObj* quadObj; /* used in drawscene */
+
+float angle = 0.0;
+// координаты вектора направления движения камеры
+float lx = 0.0f, lz = -1.0f;
+// XZ позиция камеры
+float x = 0.0f, z = 5.0f;
+/* angles for animation */
+GLUquadricObj* quadObj; /* used in animation_go */
+
+static float lmodel_oneside[] =
+{ GL_FALSE }; 
 static float lmodel_twoside[] =
 { GL_TRUE };
-static float lmodel_oneside[] =
-{ GL_FALSE };
+
+GLuint Texture[5];
 double width=800, height=600;
 int
 main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
-	quadObj = gluNewQuadric();  /* this will be used in drawScene
-								 */
+	quadObj = gluNewQuadric();  // this will be used in animation_go
+								 
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow("Two-sided lighting");
+	glutCreateWindow("Semestrovaia FILIPPOV M VIP-308");
 	glutReshapeWindow(width, height);
-	ax = 0.0;
-	ay = 0.0;
-	az = 0.0;
-
-	initLightAndMaterial();
-	glutDisplayFunc(drawScene);
+	Texture[1] = SOIL_load_OGL_cubemap
+	(
+		"posx.jpg",
+		"negx.jpg",
+		"posy.jpg",
+		"negy.jpg",
+		"posz.jpg",
+		"negz.jpg",
+		SOIL_LOAD_RGB,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS
+	);
+	Texture[0] = SOIL_load_OGL_texture("kek.png",
+		SOIL_LOAD_AUTO,
+		SOIL_CREATE_NEW_ID,
+		SOIL_FLAG_MIPMAPS);
+	initLight();
+	glutDisplayFunc(animation_go);
 	glutReshapeFunc(resize);
 	glutCreateMenu(menu);
-	glutAddMenuEntry("Motion", 3);
-	glutAddMenuEntry("Two-sided lighting", 1);
-	glutAddMenuEntry("One-sided lighting", 2);
+	glutAddMenuEntry("Start position", 1);
+	glutAddMenuEntry("End position", 2);
+	glutAddMenuEntry("One-sided lighting", 3);
 	glutAttachMenu(GLUT_RIGHT_BUTTON);
-	glutKeyboardFunc(keyboard);
+	glutKeyboardFunc(keyboard); 
+	glutSpecialFunc(processSpecialKeys);
 	glutMainLoop();
 	return 0;             /* ANSI C requires main to return int. */
 }
-
 void rand_sphr_for_tree(double x, double y, double z, double radius)
 {
 	glTranslated(x, y, z);
 	gluSphere(quadObj, radius, 20, 8);
 }
 
+GLfloat green_color[] = { 0.0, 0.9, 0.0, 0.5 };
+GLfloat green_dark_color[] = { 0.0, 0.6, 0.01, 0.5 };
+GLfloat brown_color[] = { 0.5, 0.25, 0.01, 1 };
+GLfloat white_color[] = { 1.0, 1, 1, 1 };
 void tree()
 {
 	double rad_mn_sphere;
 	double radius_spheres;
 	double size_tree;
 	glPushMatrix();
+
 	size_tree = rand()%11+8.0;
 	double ret = (double)rand() / ((double)rand()+ 0.1);
 	rad_mn_sphere =size_tree/8.0 + 2 + ret - floor(ret);
 	glRotatef(90, -1, 0, 0);
+	glColor3ub(139, 69, 19);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, brown_color);
 	gluCylinder(quadObj, ret-floor(ret)+1, 1, size_tree, 20,8);
 	glTranslated(0,0,size_tree);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green_color);
+	glColor3d(0, 0.9, 0); 
 	gluSphere(quadObj, rad_mn_sphere, 20, 8);
+	glRotated(90, 0, 0, 1);
 	for (double little_sphere = rad_mn_sphere - 0.5, y = -little_sphere; y < little_sphere; y += 1)
 	{
-		for (double circle = sqrt(pow(little_sphere, 2) - pow(y, 2)), x = -circle; x < circle; x += 0.7)
+		for (double circle = sqrt(pow(little_sphere, 2) - pow(y, 2)), x = -circle; x < circle; x += 1)
 		{
 			glPushMatrix();
 			double ret = (double)rand() / ((double)rand() + 0.1);
@@ -147,14 +129,19 @@ void christmas_tree()
 	int height = 17;
 	int width = 0;
 	glRotatef(90, -1, 0, 0);
+	glScaled(1.5, 1.5, 1.5);
+	glColor3ub(139, 69, 19);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, brown_color);
 	gluCylinder(quadObj, 1.3, 0, height, 13, 5);
+	glColor3ub(0, 100, 0);
 	while (height-4>0)
 	{
+		glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green_color);
 		width += 1;
 		int step = rand() % 4 + 2;
 		height -= step;
 		glPushMatrix();
-		glTranslated(0, 0, height-1.0);
+		glTranslated(0, 0, height);
 		gluCylinder(quadObj, width, 0, step, 10, 4);
 		glPopMatrix();
 	}
@@ -168,67 +155,176 @@ double DegToRad(double D)
 }
 void line_mebius()
 {
-	glColor3b(1, 0, 0);
 	glBegin(GL_POLYGON);
+	glNormal3f(0, 1, 0);
 	for (double u = 0; u < 360; u += 10)
 	{
 		for (double v = -1; v < 1; v += 0.03)
 		{
+			glColor3d(0.9, 0, 0);
 			glVertex3d(cos(u) * (1 + v / 2 * cos(u / 2)), sin(u)*(1+v/2*cos(u/2)), v/2*sin(u/2));
 		}
 	}
 	glEnd();
 }
-
-GLfloat VertexArrayPresent[23][36]=
+void grass()
 {
-	0, 0, 0,
-	0, 0, 1,
-	0, 1, 0,
-	0, 1, 1,
-	1, 0, 0, 
-	1, 0, 1,
-	1, 1, 0,
-	1, 1, 1,
-};
-GLubyte IndexArray[12][2]=
+	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE, GL_NORMAL_MAP);
+	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_NORMAL_MAP);
+	glEnable(GL_TEXTURE_2D);
+	glBindTexture(GL_TEXTURE_2D, Texture[0]);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
+	glEnable(GL_TEXTURE_GEN_S);
+	glEnable(GL_TEXTURE_GEN_T);
+/*
+	glRotated(0, 1, 0, 0);
+	glBegin(GL_QUADS);
+	glTexCoord2d(0, 0);
+	glVertex3d(-180, 0, -180);
+	glTexCoord2d(1, 0);
+	glVertex3d(180, 0, -180);
+	glTexCoord2d(1,1);
+	glVertex3d(180, 0, 180);
+	glTexCoord2d(0, 1);
+	glVertex3d(-180, 0, 180);
+	glEnd();*/
+}
+void SkyBox(void)
 {
-	0, 1,
-	1, 2,
-	3, 4, 
-	5, 6,
-	7, 8
-};
+	glTexGeni(GL_S, GL_TEXTURE_GEN_MODE,	GL_NORMAL_MAP);
+	glTexGeni(GL_T, GL_TEXTURE_GEN_MODE, GL_NORMAL_MAP);
+	glTexGeni(GL_R, GL_TEXTURE_GEN_MODE, GL_NORMAL_MAP);
 
+	glEnable(GL_TEXTURE_CUBE_MAP);
 
+	glBindTexture(GL_TEXTURE_CUBE_MAP, Texture[1]);
 
-void present(double size)
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP);
+	glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP);
+
+	glEnable(GL_TEXTURE_GEN_S);
+	glEnable(GL_TEXTURE_GEN_T);
+	glEnable(GL_TEXTURE_GEN_R);
+	glColor3f(1, 1, 1);
+	glutSolidSphere(200, 80, 40);
+}
+void polyana()
 {
+	glPushMatrix();
+	glClearColor(0, 0, 0, 0);
+	glRotated(-90, 1, 0, 0);
+	glRotated(45, 0, 0, 1);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green_dark_color);
+	for (int i = -170; i < 170; i += 3)
+	{
+		bool flag = true;
+		glBegin(GL_QUADS);
+		float last_top;
+		for (int j = -170; j < 170; j += 1)
+		{
+			glVertex3d(i, j, abs(sin(j)) * 3);
+			glVertex3d(i, j, 0);
+		}
+		glEnd();
+		glBegin(GL_QUADS);
+		for (int s = -170; s < 170; s += 1)
+		{
+			glVertex3d(i, s, abs(sin(s)) * 3);
+			glVertex3d(i, s, 0);
+		}
+		glEnd();
+	}
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, green_color);
+	glColor3f(0.03, 0.8, 0.04);
+	glRectd(-180, -180, 180, 180);
+	glPopMatrix();
+
+}
+void forest()
+{
+	glPushMatrix();
+	glTranslated(-60, 0, -30);
+	for (int i = 0; i < 100; i += 5)
+	{
+		double ret = (double)rand();
+		glColor3d(0, ret-floor(ret), 0);
+		glTranslated(5, 0, 40 * sin(i));
+		tree();
+		
+	}
+	glPopMatrix();
+}
+
+void lenta(double size)
+{
+	glColor3d(0.9, 0, 0);
+	glBegin(GL_QUAD_STRIP);
+	glVertex3d(-size / 2 - 0.2, 0.1, 0.5);
+	glVertex3d(-size / 2 - 0.2, 0.1, -0.5);
+	glVertex3d(size / 2 + 0.2, 0.1, 0.5);
+	glVertex3d(size / 2 + 0.2, 0.1, -0.5);
+	glVertex3d(size / 2 + 0.2, -size, 0.5);
+	glVertex3d(size / 2 + 0.2, -size, -0.5);
+	glVertex3d(-size / 2 - 0.2, -size, 0.5);
+	glVertex3d(-size / 2 - 0.2, -size, -0.5);
+	glVertex3d(-size / 2 - 0.2, 0.1, 0.5);
+	glVertex3d(-size / 2 - 0.2, 0.1, -0.5);
+	glEnd();
+}
+void present(double size, GLdouble *color)
+{
+	glPushMatrix();
+	glTranslated(0, size / 2, 0);
+	GLfloat present_color[] = { color[0],color[1],color[2],0.8 };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, present_color);
+	glColor3d(color[0],color[1],color[2]);
 	glutSolidCube(size);
 	glTranslated(0, size/2, 0);
+	GLfloat lenta_color[] = { 1,0,0,0.8 };
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, lenta_color);
+	lenta(size);
+	glRotated(90, 0, 1, 0);
+	lenta(size);
 	glRotated(90, 1, 0, 0);
 	glScaled(size/3, size/3, size/3);
 	line_mebius();
-}
-
-void
-drawScene(void)
-{
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-	glPushMatrix();
-	gluQuadricDrawStyle(quadObj, GLU_FILL);
-	glColor3f(1.0, 1.0, 0.0);
-	glRotatef(ax, 1.0, 0.0, 0.0);
-	glRotatef(-ay, 0.0, 1.0, 0.0);
-	christmas_tree();
-	glTranslated(40, 3, -10);
-	present(4);
 	glPopMatrix();
-
-	glutSwapBuffers();
 }
+ 
+
+GLfloat AnimationStart[25][2] =
+{
+	4,16,
+	8,16,
+	12,16,
+	8, 20,
+	4, 20,
+	-4, 16,
+	3, 10,
+	10, 5,
+	12, 21,
+	-5, 22,
+	-10, 13,
+	-5, 7,
+	13, 14,
+	11, 20,
+	-10, 20,
+	-13, 10,
+	-7, 4,
+	15, 6,
+	14, 16,
+	-15, 20,
+	-20, -4,
+	-5, -8,
+	18, 0,
+	19, 13,
+	-18, 14,
+
+};
+
+bool flag = false;
+
 
 void
 setMatrix(void)
@@ -237,31 +333,90 @@ setMatrix(void)
 	glLoadIdentity();
 	//glOrtho(-89.0, 89.0, -65.0, 65.0, -80.0, 80.0);
 	double aspect = (double)(width) / (double)(height);
-	gluPerspective(90.0, 1, 1.0, 1000.0);
+	gluPerspective(70.0, 1, 1.0, 1000.0);
 	glMatrixMode(GL_MODELVIEW);
-	glTranslated(0, 0, -30);
+	glTranslated(0, -8, -25);
 }
-
-int count = 0;
+int way = 0;
 
 void
-animation(void)
+animation_go(void)
 {
+	glClearColor(0.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glPushMatrix();
-	ax += 5.0;
-	ay -= 2.0;
-	az += 5.0;
-	if (ax >= 360)
-		ax = 0.0;
-	if (ay <= -360)
-		ay = 0.0;
-	if (az >= 360)
-		az = 0.0;
+	gluLookAt(x, 1.0f, z,
+		x + lx, 1.0f, z + lz,
+		0.0f, 1.0f, 0.0f);
+	draw();
+	way += 5;
+	if (way == 25)
+	{
+		way = 20;
+		glutIdleFunc(stay);
+	}
 	glPopMatrix();
-	drawScene();
-	count++;
-	if (count >= 60)
-		glutIdleFunc(NULL);
+	Sleep(200);
+	glutSwapBuffers();
+}
+
+void
+animation_back(void)
+{
+	glClearColor(0.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	gluLookAt(x, 1.0f, z,
+		x + lx, 1.0f, z + lz,
+		0.0f, 1.0f, 0.0f);
+	draw();
+	way -= 5;
+	if (way == -5)
+	{
+		way = 0;
+		glutIdleFunc(stay);
+	}
+	glPopMatrix();
+	Sleep(200);
+	glutSwapBuffers();
+}
+GLdouble ColorPresents[5][3] =
+{
+	1, 0, 1,
+	0, 1, 1,
+	1, 1, 1,
+	1, 1, 0,
+	1, 0.73, 0
+};
+
+void draw()
+{
+	glClearColor(0.0, 1.0, 1.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	polyana();
+	glDisable(GL_TEXTURE_2D);
+	glPushMatrix();
+	glLoadIdentity();
+	glTranslated(0, -90, -90);
+	glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, white_color);
+	SkyBox();
+	glPopMatrix();
+	forest();
+	gluQuadricDrawStyle(quadObj, GLU_FILL);
+	glTranslated(4, 0, 16);
+	christmas_tree();
+	glDisable(GL_TEXTURE_CUBE_MAP);
+	for (int i = 0; i < 5; i++)
+	{
+		glPushMatrix();
+		glTranslatef(AnimationStart[way + i][0], 0, AnimationStart[way + i][1]);
+		present(4,ColorPresents[i]);
+		glPopMatrix();
+	}
+}
+void stay(void)
+{
+
 }
 
 /* ARGSUSED1 */
@@ -281,20 +436,19 @@ void
 menu(int choice)
 {
 	switch (choice) {
-	case 3:
-		count = 0;
-		glutIdleFunc(animation);
+	case 1:
+		flag = true;
+		glutIdleFunc(animation_go);
 		break;
 	case 2:
+		flag = false;
+		glutIdleFunc(animation_back);
+		break;
+	case 3:
 		glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_oneside);
-		glutSetWindowTitle("One-sided lighting");
 		glutPostRedisplay();
 		break;
-	case 1:
-		glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
-		glutSetWindowTitle("Two-sided lighting");
-		glutPostRedisplay();
-		break;
+	default: stay();
 	}
 }
 
@@ -304,30 +458,58 @@ resize(int w, int h)
 	glViewport(0, 0, w, h);
 	setMatrix();
 }
+void translate()
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glPushMatrix();
+	gluLookAt(x, 1.0f, z,
+		x + lx, 1.0f, z + lz,
+		0.0f, 1.0f, 0.0f);
+	draw();
+	glPopMatrix();
+	glutSwapBuffers();
+}
+void processSpecialKeys(int key, int xx, int yy) {
+	float fraction = 0.3f;
+	switch (key) {
+	case GLUT_KEY_LEFT:
+		angle += 0.05f;
+		lx = sin(angle);
+		lz = -cos(angle);
+		translate();
+
+		break;
+	case GLUT_KEY_RIGHT:
+		angle -= 0.05f;
+		lx = sin(angle);
+		lz = -cos(angle);
+		translate();
+		break;
+	case GLUT_KEY_UP:
+		x += lx * fraction;
+		z += lz * fraction;
+		translate();
+		break;
+	case GLUT_KEY_DOWN:
+		x -= lx * fraction;
+		z -= lz * fraction;
+		translate();
+		break;
+	}
+}
 
 void
-initLightAndMaterial(void)
+initLight(void)
 {
 	static float ambient[] =
 	{ 0.1, 0.1, 0.1, 1.0 };
 	static float diffuse[] =
-	{ 0.5, 1.0, 1.0, 1.0 };
+	{ 1, 1.0, 1.0, 1.0 };
 	static float position[] =
-	{ 90.0, 90.0, 150.0, 0.0 };
-
-	static float front_mat_shininess[] =
-	{ 60.0 };
-	static float front_mat_specular[] =
-	{ 0.2, 0.2, 0.2, 1.0 };
-	static float front_mat_diffuse[] =
-	{ 0.5, 0.5, 0.28, 1.0 };
-	static float back_mat_shininess[] =
-	{ 60.0 };
-	static float back_mat_specular[] =
-	{ 0.5, 0.5, 0.2, 1.0 };
-	static float back_mat_diffuse[] =
-	{ 1.0, 0.9, 0.2, 1.0 };
-
+	{ 30.0, 90.0, 100.0, 0.0 };
+	static float position1[] =
+	{ 0, 0.0, -100.0, 0.0 };
+	
 	static float lmodel_ambient[] =
 	{ 1.0, 1.0, 1.0, 1.0 };
 
@@ -339,17 +521,18 @@ initLightAndMaterial(void)
 	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuse);
 	glLightfv(GL_LIGHT0, GL_POSITION, position);
 
-	glMaterialfv(GL_FRONT, GL_SHININESS, front_mat_shininess);
-	glMaterialfv(GL_FRONT, GL_SPECULAR, front_mat_specular);
-	glMaterialfv(GL_FRONT, GL_DIFFUSE, front_mat_diffuse);
-	glMaterialfv(GL_BACK, GL_SHININESS, back_mat_shininess);
-	glMaterialfv(GL_BACK, GL_SPECULAR, back_mat_specular);
-	glMaterialfv(GL_BACK, GL_DIFFUSE, back_mat_diffuse);
 
 	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
-	glLightModelfv(GL_LIGHT_MODEL_TWO_SIDE, lmodel_twoside);
 
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glLightfv(GL_LIGHT1, GL_AMBIENT, ambient);
+	glLightfv(GL_LIGHT1, GL_DIFFUSE, diffuse);
+	glLightfv(GL_LIGHT1, GL_POSITION, position1);
+
+
+	glLightModelfv(GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+	glEnable(GL_LIGHT1);
 	glShadeModel(GL_SMOOTH);
 }
